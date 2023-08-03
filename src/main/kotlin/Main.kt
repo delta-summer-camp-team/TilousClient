007f2e31ca99a860b7ff4bd6bfbb8f6763e5b3eb
Markmain <arotter003@gmail.com>
2023-08-03 14:57:46 +0100

    fixed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 import com.delta.AppConfig
 import com.delta.Application
 
-fun main(args: Array<String>) {
+fun main() {
     val serverAddress = "http://192.168.178.48"
-    //val playersName = args[0]
+
 
     val app = Application(AppConfig("A", serverAddress))
     app.start()
 }
+
