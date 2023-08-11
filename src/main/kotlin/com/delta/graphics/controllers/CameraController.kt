5bf29fad8279a6f27a9b400b96e833288d4b5460
Alisa1543 <alisa.zikeeva@gmail.com>
2023-08-11 14:29:30 +0200

    cameraController

--- src/main/kotlin/com/delta/graphics/controllers/CameraController.kt
+++ src/main/kotlin/com/delta/graphics/controllers/CameraController.kt
@@ -1,6 +1,7 @@
 package com.delta.graphics.controllers
 
 import com.badlogic.gdx.InputAdapter
+import com.badlogic.gdx.math.Vector3
 import com.delta.graphics.geometry.Camera
 
 // camera.position
@@ -8,7 +9,7 @@ class CameraController(
     private val camera: Camera
 ) : InputAdapter() {
 
-    private val lastPosition = camera.getPosition()
+    private val lastPosition = Vector3()
     private var positionOnTouch = Pair(0, 0)
 
     override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
@@ -19,10 +20,9 @@ class CameraController(
 
     override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
         val newPosition = camera.screenToWorld(screenX, screenY)
+        lastPosition.sub(newPosition)
         camera.translate(lastPosition)
-        val differenceVectors = lastPosition.sub(newPosition)
-        //camera.updatePosition(differenceVectors)
-        camera.translate(differenceVectors)
+        lastPosition.set(camera.screenToWorld(screenX, screenY))
         return true
     }
 
